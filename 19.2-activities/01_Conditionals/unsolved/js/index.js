66bbf2323d34b6b9b2e7dce40cea23f7765f8c4b
emily-leibowitz <111251321+emily-leibowitz@users.noreply.github.com>
2023-01-12 18:30:23 -0600

    Update index.js

--- 19.2-activities/01_Conditionals/unsolved/js/index.js
+++ 19.2-activities/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,10 @@
 $('.tab').on("click", function(){
-  
+  if ($(this).hasClass("tab1")) {
+    $(".tab1").addClass("active")
+    $(".tab2").removeClass("active")
+  }
+  else if ($(this).hasClass("tab2")) {
+    $(".tab1").removeClass("active")
+    $(".tab2").addClass("active");
+  }
 });
